197b628098a2cc204b63f17f94a788cdac05a1f6
bhavdip <bhavdipbaldaniya2@gmail.com>
2024-10-09 18:32:36 +0530

    adminsiteManage Plans completed

--- src/app/manageplans/page.js
+++ src/app/manageplans/page.js
@@ -1,10 +1,13 @@
+'use client'
 import React from 'react'
 import Manageplans from './Component/Manageplans'
+import PlanFeatures from './Component/Planfeatures'
 
 const page = () => {
     return (
         <>
-            <Manageplans />
+            {/* <Manageplans /> */}
+            <PlanFeatures />
         </>
     )
 }
